9b1efa0394e0cda7f4facdbb4713c731c341e7b6
LukieD4 <lukiedoggo04@gmail.com>
2025-09-08 16:52:32 +0100

    First commit (w/ translations)

--- src/main/java/com/lukiedoggo/beefjerkymod/BeefJerkyMod.java
+++ src/main/java/com/lukiedoggo/beefjerkymod/BeefJerkyMod.java
@@ -1,6 +1,8 @@
 package com.lukiedoggo.beefjerkymod;
 
 import com.lukiedoggo.beefjerkymod.registry.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -9,6 +11,16 @@ public class BeefJerkyMod {
     public static final String MOD_ID = "beefjerkymod";
 
     public BeefJerkyMod() {
-        ModItems.register(FMLJavaModLoadingContext.get().getModEventBus());
+        var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        ModItems.register(modEventBus);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.COOKED_JERKY);
+            event.accept(ModItems.CRISPY_JERKY);
+        }
     }
 }
--- src/main/java/com/lukiedoggo/beefjerkymod/registry/ModItems.java
+++ src/main/java/com/lukiedoggo/beefjerkymod/registry/ModItems.java
@@ -2,7 +2,6 @@ package com.lukiedoggo.beefjerkymod.registry;
 
 import com.lukiedoggo.beefjerkymod.BeefJerkyMod;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,25 +10,23 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
-        DeferredRegister.create(ForgeRegistries.ITEMS, BeefJerkyMod.MOD_ID);
+            DeferredRegister.create(ForgeRegistries.ITEMS, BeefJerkyMod.MOD_ID);
 
     public static final RegistryObject<Item> COOKED_JERKY = ITEMS.register("cooked_flesh",
-        () -> new Item(new Item.Properties()
-            .tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder()
-                .nutrition(3)
-                .saturationMod(0.6F)
-                .meat()
-                .build())));
+            () -> new Item(new Item.Properties()
+                    .food(new FoodProperties.Builder()
+                            .nutrition(3)
+                            .saturationMod(0.6F)
+                            .meat()
+                            .build())));
 
     public static final RegistryObject<Item> CRISPY_JERKY = ITEMS.register("crispy_flesh",
-        () -> new Item(new Item.Properties()
-            .tab(CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder()
-                .nutrition(4)
-                .saturationMod(0.8F)
-                .meat()
-                .build())));
+            () -> new Item(new Item.Properties()
+                    .food(new FoodProperties.Builder()
+                            .nutrition(4)
+                            .saturationMod(0.8F)
+                            .meat()
+                            .build())));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
